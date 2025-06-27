da198b602eaeb1339109ac3b892a8641e4622f91
Julen37 <julen.gouchault@gmail.com>
2025-06-27 09:28:32 +0200

    first commit exo 9 Hello World

---
+++ bonjour.js
@@ -0,0 +1,3 @@
+//exo 9 Dire bonjour au monde
+let bonjour = document.getElementById("helloWorld").textContent; // permet de recuperer l'id dans le html pour savoir où on est pour ensuite prendre le contenu du texte pour l'afficher avec le alert
+alert (bonjour);
