83140354d252b2386c283244fceae4fe76c6c966
Julen37 <julen.gouchault@gmail.com>
2025-06-30 12:08:05 +0200

    exo 10 surbrillance au clic js

--- JS-FORM-1/index.js
+++ JS-FORM-1/index.js
@@ -19,17 +19,30 @@
 // element.classList.remove("hidden");
 
 //exo 10 cacher les spoilers
-let elements = document.getElementsByClassName("spoiler");
-for (i=0; i<elements.length ; i++){
-    let element = elements[i];
-    element.classList.remove("hidden");
-}
-let cliquable = document.getElementById('spoilerButton').onclick = function HideSpoiler() {
-    for (i=0; i<elements.length ; i++){
-    let element = elements[i];
-    element.classList.add("hidden");
+// let elements = document.getElementsByClassName("spoiler");
+// let cliquable = document.getElementById('spoilerButton').onclick = function HideSpoiler() {
+//     for (let i = 0; i < elements.length ; i++){ // tant que i est plus petit que la longueur du tableau de la class spoiler alors il incremente pour chercher le prochaine
+//     let element = elements[i]; 
+//     element.classList.add("hidden"); //ajoute la class hidden déjà créé dans le css aux elements du tableau de la class spoiler
+//     }
+// }
+
+// Exo 10 Surbrillance au clic
+//mettre en jaune le second texte
+// let element = document.getElementById("second");
+// element.style.backgroundColor = "yellow";
+
+//mettre en jaune quand on appui sur le seconc
+// let element = document.getElementById("second");
+// document.getElementById("second").onclick = function yellowClick(){
+//     element.style.backgroundColor = "yellow";
+// }
+
+// mettre en jaune quand on appui sur n'importe quel p
+let element = document.getElementsByTagName("p") = function yellowClick(){
+    for (var i = 0; i < element.length; i++){
+        let test = element[i];
+        test.classList.onclick("p");
+        EventTarget(test.classList.onclick("p")).style.backgroundColor = "yellow";
     }
 }
-
-
-
